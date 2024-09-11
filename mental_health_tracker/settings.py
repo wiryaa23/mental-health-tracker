ffe1a445c5e5bd5b2984b91bf53f53f710777c49
wiryaa23 <wiryawdk@gmail.com>
2024-09-11 14:11:25 +0700

    finish tutorial 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://http://wirya-dharma-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://http://wirya-dharma-mentalhealthtracker.pbp.cs.ui.ac.id/"]
