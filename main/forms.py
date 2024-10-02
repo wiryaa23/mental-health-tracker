2f9dbbb29c9eae3d81163295b7aff7de4e803070
wiryaa23 <wiryawdk@gmail.com>
2024-10-02 14:14:35 +0700

    finish tutorial 5 part 2

--- main/forms.py
+++ main/forms.py
@@ -7,10 +7,10 @@ class MoodEntryForm(ModelForm):
         model = MoodEntry
         fields = ["mood", "feelings", "mood_intensity"]
 
-        def clean_mood(self):
-            mood = self.cleaned_data["mood"]
-            return strip_tags(mood)
+    def clean_mood(self):
+        mood = self.cleaned_data["mood"]
+        return strip_tags(mood)
 
-        def clean_feelings(self):
-            feelings = self.cleaned_data["feelings"]
-            return strip_tags(feelings)
+    def clean_feelings(self):
+        feelings = self.cleaned_data["feelings"]
+        return strip_tags(feelings)
